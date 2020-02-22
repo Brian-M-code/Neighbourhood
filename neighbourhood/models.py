4395f24e1dfe61a4620224c154f4e618baef91bd
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-23 00:37:54 +0300

    added post and category class models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -58,8 +58,20 @@ class Business(models.Model):
     def search_by_name(cls, search_term):
         business = cls.objects.filter(title__icontains=search_term)
         return business
+    
+class Post(models.Model):
+    user = models.ForeignKey(Profile, related_name='profile')
+    post = models.CharField(max_length=255)
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name='posts')
         
-
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def save_category(self):
+        self.save()
+    def delete_category(self):
+        self.delete()
+    def __str__(self):
+        return self.name
 
 
     
