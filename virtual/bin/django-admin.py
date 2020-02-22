a60f5ceea412cc0a9965387b326df0db3fc4a6c6
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-22 22:42:51 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mutumas/Documents/python/Django/Django-IPs/Neighbourhood./virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
