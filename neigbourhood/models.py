759ec4d932f06dfbfdf0b2dc80a56fa0bd836e24
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-22 23:49:15 +0300

    added save delete methods to the business model

--- neigbourhood/models.py
+++ neigbourhood/models.py
@@ -8,9 +8,9 @@ class Business(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     neighbourhood = models.ForeignKey(Neighbourhood, related_name='businesses')
     email = models.EmailField(max_length=30)
+    image = models.ImageField(upload_to='bizimage/')
     
     def __str__(self):
         return self.name
@@ -18,5 +18,13 @@ class Business(models.Model):
     def get_absolute_url(self):
         return reverse('neighbourhood_detail', args=[str(self.id)])
     
-
+    def save_business(self):
+        self.save()
     
+    def delete_business(self):
+        self.delete()
+        
+    @classmethod
+    def search_by_name(cls, search_term):
+        business = cls.objects.filter(title__icontains=search_term)
+        return business
