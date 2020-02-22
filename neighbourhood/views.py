edd2478af49092cd9a802b6d5822cee85917b236
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-23 01:06:34 +0300

    added a signUp view function

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
 def index(request):
     neighbourhood = Neighbourhood.objects.all()
     context = {
     'neighbourhood': neighbourhood,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', context,{"neighbourhood":neighbourhood})
+
+def neighbourhood(request, pk):
+    current_user = request.user
+    neighbourhood = Neighbourhood.objects.get(pk=pk)
+    business = Business.get_business(business.id)
+    form = Post(request.Post)
+    
+    if request.method == 'POST':
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.user = current_user
+            post.post = post
+            post.post_id = post.id
+            post.save()
+            return redirect('index')
+    
+    else:
+        fo
