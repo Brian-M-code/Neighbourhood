6e9443172512e170c3a70aec7c651bc59df6af96
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-23 00:53:55 +0300

    added urls file and created an index view function

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    neighbourhood = Neighbourhood.objects.all()
+    context = {
+    'neighbourhood': neighbourhood,
+    }
+    return render(request, 'index.html', context)
