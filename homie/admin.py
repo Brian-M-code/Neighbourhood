393d0d7346ee882717af19109c6ef8b38d29e9e3
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-26 15:39:34 +0300

    correct model spelling

--- homie/admin.py
+++ homie/admin.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from .models import Neighbourhood, Business, Posts
+from .models import Neighbourhood, Business, Post
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Business)
-admin.site.register(Posts)
+admin.site.register(Post)
